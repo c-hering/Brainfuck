94f1c6b1921d4b4a5ff64accc9961cea2affa5a7
An0rak <phos4es@gmail.com>
2017-07-31 14:50:34 -0400

    ADDED: new functions TODO: fix new functions

--- Brainfuck/Source.cpp
+++ Brainfuck/Source.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 
 void setShellSize();
+void prompt(int delay, string text);
+void helpResponse();
+void readFile(string fpath);
 
 void setShellSize() {
 	HWND console = GetConsoleWindow();
@@ -15,6 +18,34 @@ void setShellSize() {
 	
 	GetWindowRect(console, &r);
 	MoveWindow(console, r.left, r.top, 800, 500, TRUE);
+}
+
+void prompt(int delay, string text) {
+	for (int i = 0; i < text.length(); i++) {
+		cout << text[i];
+		Sleep(delay);
+	}
+	cout << endl;
+}
+
+void helpResponse() {
+	cout << " I need help too so I dont think I can help you..." << endl;
+}
+
+void readFile(string fpath) {
+	ifstream fin;
+	string line;
+	fin.open(fpath);
+
+	if (fin.is_open()) {
+		cout << "Reading File From: " << fpath << endl;
+		while (getline(fin, line)) {
+			cout << " #  " << line << endl;
+		}
+	}
+	else {
+		cout << "Couldn't Find File" << endl;
+	}
 }
 
 int main() {
@@ -60,8 +91,39 @@ int main() {
 		  \/_____/   \/_/ /_/   \/_/\/_/   \/_/   \/_/ \/_/   \/_/     \/_____/   \/_____/   \/_/\/_/
 
 	*/
-
-	setShellSize();
+	//Vars
 	asciiImg title;
+	string input;
+	bool promptLoop = true;
+
+	//Resizing
+	setShellSize();
+	
+	//Main Menu, Title
 	title.printImg("title.txt");
+	cout << endl << "Developed By: c-hering, toastyyyyy, whatever u wanna call me" << endl;
+	cout << "Checkout my github at: https://github.com/c-hering" << endl << endl;
+	while (promptLoop) {
+		prompt(50, "**Please Input SOMETHING**");
+		cout << ">> ";
+		getline(cin, input);
+		if (input == "help") {
+			helpResponse();
+		}
+		else if (input == "exit") {
+			exit(0);
+		}
+		else if (input == "read file") {
+			string path;
+			system("cd");
+			system("dir");
+			prompt(10, "Reading File :: Please Give a File Path");
+			cout << "	>>";
+			getline(cin, path);
+			readFile(path);
+		}
+		else {
+			cout << "I dont know what to do with this information..." << endl << "Hello World" << endl;
+		}
+	}
 }
